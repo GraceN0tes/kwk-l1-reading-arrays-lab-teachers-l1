aaf6e24fce347c6ef4c67aa8b6ea9fec7fcec44b
Grace Park <parkgra3@gmail.com>
2018-08-08 16:23:52 +0000

    automatically backed up by learn

--- readingarraysnotes.rb
+++ readingarraysnotes.rb
@@ -1,3 +1,38 @@
+$STUDENT_NAMES = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+
+def first_student_by_index
+  # Write a solution that returns the first student in the array student_names
+  puts $STUDENT_NAMES[0]
+end
+first_student_by_index
+
+def fourth_student_by_index
+  # Write a solution that returns the first student in the array student_names
+end
+
+def last_student_by_index
+  # Write a solution that returns the first student in the array student_names
+end
+
+def first_student_by_method
+  # Write a solution that returns the first student using the built in .first method
+end
+
+def last_student_by_method
+  # Write a solution that returns the first student using the built in .last method
+end
+
+def first_second_and_third_students
+  # Write a solution that returns the first, second and third students
+end
+
 STUDENT_NAMES = [
   "Adele",
   "Beyoncé",
